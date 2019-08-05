2d7b17cab9e03cef18a193699d5cbad6055f9bd0
MickSheridan24 <micksheridan.24@gmail.com>
2019-08-04 22:43:42 -0400

    chords table

---
+++ back/db/migrations/20190804224003_create_chords_table.js
@@ -0,0 +1,18 @@
+exports.up = function(knex) {
+  return knex.schema.hasTable("chords").then(exists => {
+    if (!exists) {
+      return knex.schema.createTable("chords", tbl => {
+        tbl.increments();
+        tbl.string("note");
+        tbl.string("aspect");
+        tbl.integer("noteNum");
+        tbl.integer("line");
+        tbl.integer("offset");
+      });
+    }
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("chords");
+};
